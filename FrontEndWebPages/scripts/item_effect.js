2adade5281c6c534622758af0cf07d1b66098d1a
Zhao Yuqi <zyqscott@gmail.com>
2013-05-06 10:11:53 +0800

    Revert "item页面效果预览"
    This reverts commit c63fe1f91a2011acd55c0c8e1501aa6d9062eb65.

--- FrontEndWebPages/scripts/item_effect.js
+++ FrontEndWebPages/scripts/item_effect.js
@@ -1,30 +1,3 @@
 /**
  * @author Ben
  */
-
-var isBottomBarDisplayed = false;
-
-$(document).ready(function() {
-    $("#frame").click(function() {
-        console.log(document.getElementsByTagName("header")[0].getBoundingClientRect().top);
-    });
-
-    $(window).scroll(function() {
-        if (document.getElementsByTagName("header")[0].getBoundingClientRect().top <= -60) {
-            $("#content_fixed").css("position", "fixed");
-            $("#content_fixed").css("top", "40px");
-            if (isBottomBarDisplayed == false) {
-                isBottomBarDisplayed = true;
-                $("footer").fadeIn('slow');
-            }
-        } else {
-            $("#content_fixed").css("position", "relative");
-            $("#content_fixed").css("top", "0");
-            if (isBottomBarDisplayed == true) {
-                isBottomBarDisplayed = false;
-                $("footer").fadeOut('slow');
-            }
-
-        }
-    });
-});
