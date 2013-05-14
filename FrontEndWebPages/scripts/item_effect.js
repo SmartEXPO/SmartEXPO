10e357af115aed581bb30080c3ab7df43a0e39b5
Zhihao <lzh727@126.com>
2013-05-14 22:21:22 +0800

    minor bug fixed

--- FrontEndWebPages/scripts/item_effect.js
+++ FrontEndWebPages/scripts/item_effect.js
@@ -13,15 +13,15 @@ $(document).ready(function() {
 
     $(window).scroll(function() {
         if (document.getElementsByTagName("header")[0].getBoundingClientRect().top <= -60) {
-            $("#content_fixed").css("position", "fixed");
-            $("#content_fixed").css("top", "40px");
+            $(".content_fixed").css("position", "fixed");
+            $(".content_fixed").css("top", "40px");
             if (isBottomBarDisplayed == false) {
                 isBottomBarDisplayed = true;
                 $("footer").fadeIn('slow');
             }
         } else {
-            $("#content_fixed").css("position", "relative");
-            $("#content_fixed").css("top", "0");
+            $(".content_fixed").css("position", "");
+            $(".content_fixed").css("top", "");
             if (isBottomBarDisplayed == true) {
                 isBottomBarDisplayed = false;
                 $("footer").fadeOut('slow');
