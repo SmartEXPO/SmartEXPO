794a5f0f28b63e71b9ad1ebffc2176f3a801baaf
Zhihao <lzh727@126.com>
2013-05-14 23:30:03 +0800

    comment模块添加

--- FrontEndWebPages/scripts/item_effect.js
+++ FrontEndWebPages/scripts/item_effect.js
@@ -79,13 +79,41 @@ $(document).ready(function() {
         });
     });
 
+    $("#comment_name_box").focus(function() {
+        $(this).val("");
+    });
+
+    $("#comment_content_box").focus(function() {
+        $(this).val("");
+
+    });
+
     $("#comment_img").click(function() {
         hideSearchPane();
         if (isCommentBarDisplayed == false) {
             isCommentBarDisplayed = true;
-            $("#comment").animate({
-                "width" : "250px",
+            $("#comment_input").css("display", "inline-block");
+            $("#comment_input").animate({
+                width : "360px",
             }, 500);
+            $("#comment_name_box").animate({
+                width : "100px",
+            }, 150, function() {
+                $("#comment_content_box").animate({
+                    width : "235px",
+                }, 320);
+            });
+            $("#comment").animate({
+                "width" : "400px",
+            }, 500, function() {
+                $("#comment").animate({
+                    "height" : "400px",
+                }, 500, function() {
+                    $("#users_comment").css("display", "block");
+                });
+            });
+            $("#comment_content_box").val("Your Comment");
+            $("#comment_name_box").val("Name");
         } else {
             hideCommentPane();
         }
@@ -93,24 +121,41 @@ $(document).ready(function() {
 
     function hideCommentPane() {
         isCommentBarDisplayed = false;
+        $("#comment_input").animate({
+            width : "0px",
+        }, 500, function() {
+            $("#comment_input").css("display", "none");
+        });
+
+        $("#comment_content_box").animate({
+            width : "0px",
+        }, 360, function() {
+            $("#comment_name_box").animate({
+                width : "0px",
+            }, 150);
+        });
+
+        $("#users_comment").css("display", "none");
+
         $("#comment").animate({
             "width" : "40px",
             'height' : '40px',
         }, 500);
     }
-    
+
+
     $('.inbox').focus(function() {
         $(this).val("");
-        $(this).css('color','black');
+        $(this).css('color', 'black');
         if ($(this).attr('name') === "password") {
-            $(this).attr('type','password');
+            $(this).attr('type', 'password');
         }
     });
 
 });
 
 function popupLogin() {
-    $('#overall_shade').fadeTo('fast',0.4);
+    $('#overall_shade').fadeTo('fast', 0.4);
     $('#login_panel').fadeIn('fast');
 }
 
