415676da2848b6018ec5f3042f23071d17aba62c
Boy Lee <boylee1111@gmail.com>
2013-05-15 16:39:39 +0800

    comment finish

--- SmartEXPO_Proj_new/src/java/com/smartexpo/managedbean/LoginManagedBean.java
+++ SmartEXPO_Proj_new/src/java/com/smartexpo/managedbean/LoginManagedBean.java
@@ -23,6 +23,7 @@ public class LoginManagedBean {
     
     private String username;
     private String password;
+    private boolean status;
 
     /**
      * @return the username
@@ -50,5 +51,19 @@ public class LoginManagedBean {
      */
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    /**
+     * @return the status
+     */
+    public boolean isStatus() {
+        return status;
+    }
+
+    /**
+     * @param status the status to set
+     */
+    public void setStatus(boolean status) {
+        this.status = status;
     }
 }
