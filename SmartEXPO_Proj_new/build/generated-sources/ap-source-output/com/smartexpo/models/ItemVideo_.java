c3b7a32db6d01809e26acc44279421701de4299b
Boy Lee <boylee1111@gmail.com>
2013-05-15 15:35:48 +0800

    Test

--- SmartEXPO_Proj_new/build/generated-sources/ap-source-output/com/smartexpo/models/ItemVideo_.java
+++ SmartEXPO_Proj_new/build/generated-sources/ap-source-output/com/smartexpo/models/ItemVideo_.java
@@ -6,7 +6,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.3.2.v20111125-r10461", date="2013-05-15T13:39:58")
+@Generated(value="EclipseLink-2.3.2.v20111125-r10461", date="2013-05-15T15:35:35")
 @StaticMetamodel(ItemVideo.class)
 public class ItemVideo_ { 
 
