b177359e9d25aeec14c0da50acb2211302e3cb42
Boy Lee <boylee1111@gmail.com>
2013-05-15 20:46:30 +0800

    little changes

--- SmartEXPO_Proj_new/build/generated-sources/ap-source-output/com/smartexpo/models/ItemVideo_.java
+++ SmartEXPO_Proj_new/build/generated-sources/ap-source-output/com/smartexpo/models/ItemVideo_.java
@@ -6,7 +6,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.3.2.v20111125-r10461", date="2013-05-15T16:55:48")
+@Generated(value="EclipseLink-2.3.2.v20111125-r10461", date="2013-05-15T20:46:23")
 @StaticMetamodel(ItemVideo.class)
 public class ItemVideo_ { 
 
--- SmartEXPO_Proj_new/src/java/com/smartexpo/managedbean/LoginManagedBean.java
+++ SmartEXPO_Proj_new/src/java/com/smartexpo/managedbean/LoginManagedBean.java
@@ -19,6 +19,8 @@ public class LoginManagedBean {
      * Creates a new instance of LoginManagedBean
      */
     public LoginManagedBean() {
+        username = "Username";
+        password = "Password";
     }
     
     private String username;
