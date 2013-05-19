e76ef82abac4a9277bf361bf125172cf9042ec8e
Boy Lee <boylee1111@gmail.com>
2013-05-19 16:55:05 +0800

    采用ajax完善login

--- SmartEXPO_Proj_new/build/generated-sources/ap-source-output/com/smartexpo/models/ItemAuthor_.java
+++ SmartEXPO_Proj_new/build/generated-sources/ap-source-output/com/smartexpo/models/ItemAuthor_.java
@@ -6,7 +6,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.3.2.v20111125-r10461", date="2013-05-18T20:42:35")
+@Generated(value="EclipseLink-2.3.2.v20111125-r10461", date="2013-05-19T16:54:36")
 @StaticMetamodel(ItemAuthor.class)
 public class ItemAuthor_ { 
 
