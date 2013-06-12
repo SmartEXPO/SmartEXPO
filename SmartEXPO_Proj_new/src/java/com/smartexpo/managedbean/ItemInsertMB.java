94ca7172d076ad3f264763ad78fc949e84e4114a
Boy Lee <boylee1111@gmail.com>
2013-06-12 21:39:09 +0800

    cc

--- SmartEXPO_Proj_new/src/java/com/smartexpo/managedbean/ItemInsertMB.java
+++ SmartEXPO_Proj_new/src/java/com/smartexpo/managedbean/ItemInsertMB.java
@@ -271,6 +271,14 @@ public class ItemInsertMB implements Serializable {
     }
 
     public void persist() {
+        Logger.getLogger(ItemInsertMB.class.getName()).log(Level.WARNING, "content0 = {0}", desContent);
+        desContent = desContent.replaceAll("\n", "<br />");
+        Logger.getLogger(ItemInsertMB.class.getName()).log(Level.WARNING, "content1 = {0}", desContent);
+        desContent = desContent.replaceAll("\r", " ");
+        Logger.getLogger(ItemInsertMB.class.getName()).log(Level.WARNING, "content2 = {0}", desContent);
+        desContent = desContent.replaceAll("'", "#");
+        Logger.getLogger(ItemInsertMB.class.getName()).log(Level.WARNING, "content3 = {0}", desContent);
+
         try {
             utx.begin();
 
@@ -280,7 +288,6 @@ public class ItemInsertMB implements Serializable {
             item.setArea(itemArea);
 
             em.persist(item);
-
             Description description = new Description();
             description.setTitle(desTitle);
             description.setContent(desContent);
