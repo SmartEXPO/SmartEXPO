9f171f5d63c2f8124ffcb83d32754f21b9ed3fb2
Zhihao <lzh727@126.com>
2013-06-16 16:31:17 +0800

    map info updated

--- SmartEXPO_Proj_new/web/resources/theme1/1_0/scripts/map_effect.js
+++ SmartEXPO_Proj_new/web/resources/theme1/1_0/scripts/map_effect.js
@@ -5,18 +5,21 @@
  */
 function initMapInteraction() {
     shadow = new createjs.Shadow("#6e6e6e", 3, 3, 10);
-
+    $("#areaname").css('display','none');
     for (var i = 0; i < area.length; i++) {
         area[i].set({id: (i + 1)}); //为每个area设置id，编号从1开始
         area[i].addEventListener("mouseover", function(e) {
             e.target.set({
                 shadow: shadow,
             });
+            document.getElementById("areaname").innerHTML = "No." + e.target.id + " Area";
+            $("#areaname").clearQueue().fadeIn('fast');
         });
         area[i].addEventListener("mouseout", function(e) {
             e.target.set({
                 shadow: null,
             });
+            $("#areaname").css('display', 'none');
         });
         area[i].addEventListener("click", function(e) {
             //alert(e.target.id);
