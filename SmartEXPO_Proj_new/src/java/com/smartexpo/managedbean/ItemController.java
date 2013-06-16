e4f984168dec5bfc2426002819bdd87814ba3e1b
Boy Lee <boylee1111@gmail.com>
2013-06-17 03:08:48 +0800

    bux fixed

--- SmartEXPO_Proj_new/src/java/com/smartexpo/managedbean/ItemController.java
+++ SmartEXPO_Proj_new/src/java/com/smartexpo/managedbean/ItemController.java
@@ -150,8 +150,6 @@ public class ItemController implements Serializable {
         gi = new GetInfo(emf, utx);
 
         initialCommentsList();
-
-        Logger.getLogger(ItemController.class.getName()).log(Level.WARNING, "hashtml = {0}", itemBean.isHasHtml());
     }
 
     /**
